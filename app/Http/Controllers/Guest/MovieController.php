ecd2d435d1ea116587c3bd8c1c1dd41fedfe0250
nicomanunta <nic.manun@gmail.com>
2024-02-14 15:28:02 +0100

    pusho errore

--- app/Http/Controllers/Guest/MovieController.php
+++ app/Http/Controllers/Guest/MovieController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
     public function index(){
+        $movie = Movie::all();
         return view('movies.index');
     }
 }
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -2,7 +2,12 @@
 @section('movie-content')
     <div class="container">
         <div class="row">
-            <div class="col-12">CIAO</div>
+            @foreach ($movies as $movie)
+                {{$movie['title']}}
+            @endforeach
+            <div class="col-12">
+
+            </div>
         </div>
     </div>
 @endsection
