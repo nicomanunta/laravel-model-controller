a26895a4999a6c5cbbb3c887b43c39cb2de61e54
nicomanunta <nic.manun@gmail.com>
2024-02-14 14:50:44 +0100

    aggiunta home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-
-    <main class="bg-light">
-        
-    </main>
-
-</body>
-
-</html>
+@extends('layout.app')
+@section('content')
+    <div class="container vh-100  d-flex align-items-center justify-content-center  ">
+        <div class="row">
+            <div class="col-12 ">
+                <a href="" type="button" class="btn btn-warning btn-padding">FILM</a>
+            </div>
+        </div>
+    </div>
+@endsection
