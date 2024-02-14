ccf36520987850ae2bed755094b88bc67a5a2b82
nicomanunta <nic.manun@gmail.com>
2024-02-14 16:05:52 +0100

    sistemato il layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,11 @@
 @extends('layout.app')
 @section('content')
-    <div class="container vh-100  d-flex align-items-center justify-content-center  ">
-        <div class="row">
-            <div class="col-12 ">
-                <a href="{{route('index_movies')}}" type="button" class="btn btn-warning btn-padding">FILM</a>
+    <div class='bg-black'>
+        <div class="container vh-100  d-flex align-items-center justify-content-center ">
+            <div class="row">
+                <div class="col-12 ">
+                    <a href="{{route('index_movies')}}" type="button" class="btn btn-warning btn-padding">FILM</a>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,21 +1,25 @@
 @extends('layout.movie')
 @section('movie-content')
-    <div class="container">
-        <div class="row">
-            @foreach ($movies as $movie)
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                  <h5 class="card-title">{{$movie['title']}}</h5>
-                  <h6 class="card-subtitle mb-2 text-body-secondary">{{$movie['original_title']}}</h6>
-                  <span class="card-text">{{$movie['nationality']}}</span>
-                  <p class="card-text">{{$movie['date']}}</p>
-                  <p class="card-text">{{$movie['vote']}}</p>
-                  
+    <div class='bg-black'>
+        <div class="container pt-5">
+            <div class="row ">
+                @foreach ($movies as $movie)
+                <div class="col-4 mb-4 d-flex justify-content-center">
+                    <div class="card border-card" style="width: 18rem;">
+                        <div class="card-body min-height ">
+                            <h5 class="card-title pt-3">{{$movie['title']}}</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary pb-5 pt-2">{{$movie['original_title']}}</h6>
+                            <span class="card-text">Nazionalità: {{$movie['nationality']}}</span>
+                            <br>
+                            <span class="card-text">Data di uscita: {{$movie['date']}}</span>
+                            <p class="card-text">Voto: {{$movie['vote']}}</p>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            @endforeach
-            <div class="col-12">
+                @endforeach
+                <div class="col-12">
 
+                </div>
             </div>
         </div>
     </div>
